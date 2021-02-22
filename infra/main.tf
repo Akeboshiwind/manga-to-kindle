20540a026da71066f181d70439eb1b9663e3ac34
Oliver Marshall <olivershawmarshall@gmail.com>
2021-02-22 15:12:21 +0000

    Enable debug logs

--- infra/main.tf
+++ infra/main.tf
@@ -152,6 +152,7 @@ resource "aws_lambda_function" "lambda" {
       BOT_HOOK_PATH = "/v1/manga_to_kindle/"
       # TODO: Make work without cycle
       #BOT_HOOK_PATH = "/${aws_api_gateway_stage.stage.stage_name}/${aws_api_gateway_resource.resource.path_part}/"
+      DEBUG = "*"
     }
   }
 }
