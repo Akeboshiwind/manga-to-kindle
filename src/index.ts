3749358d3c95624687ebe7239cb2d2b2aca1eb36
Oliver Marshall <olivershawmarshall@gmail.com>
2021-03-12 11:22:51 +0000

    Improve comments

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,9 @@ const bot = new Telegraf(config.bot.token, {
 });
 
 bot.hears(manga.chapterURLRegex, async (ctx) => {
-    // Get manga info
     debug("Mangadex chapter link detected");
+
+    // >> Get manga info
     const chapterId = parseInt(ctx.match[1], 10);
     const info = await manga.getMangaInfo(chapterId);
     const baseFilename = info.mangaName;
@@ -25,16 +26,22 @@ bot.hears(manga.chapterURLRegex, async (ctx) => {
     const zipFilename = `${baseFilename}.zip`;
     debug("Got manga info");
 
+    // >> Download page images
     const pageBuffs = await download.downloadPages(info.pageLinks);
     debug("Got pages");
+
+    // >> Build PDF out of page images
     const pdfStream = await pdf.buildPDF(pageBuffs);
     debug("Got build pdf");
 
+    // >> Zip
     const zipStream = await zip.zipStream(pdfStream, pdfFilename);
     debug("Got zip");
 
+    // >> Send email
     await email.emailMangaPDF(zipStream, zipFilename);
 
+    // >> Replying
     debug("Replying to message with confirmation");
     ctx.reply(format("Successfully packaged and sent %s", zipFilename));
 });
