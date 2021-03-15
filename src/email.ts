5e8e69570617c8d34da0eb220e560fad20011bee
Oliver Marshall <olivershawmarshall@gmail.com>
2021-03-15 13:34:02 +0000

    Fix a bug where emails with only attachments aren't recieved by the kindle

--- src/email.ts
+++ src/email.ts
@@ -17,6 +17,14 @@ export async function emailStream(fileStream: NodeJS.ReadableStream, filename: s
     await transporter.sendMail({
         from: config.email.from,
         to: config.email.to,
+        // This fixes a bug where either:
+        // - Nodemailer generates incorrect SMTP
+        // - The kindle email client doesn't know how to parse this edge case
+        // By inserting some text we can change the SMTP format slightly so
+        // that the kindle will parse it.
+        // See the following for a re-creation of the issue:
+        // https://github.com/ake-temp/send-demo
+        text: "fixes bug",
         attachments: [{
             filename,
             content: new Readable().wrap(fileStream),
